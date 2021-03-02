90e3ae86c5170129fe6c612be59db3dffaa5695a
Pinolillo <68524242+Pinolillo@users.noreply.github.com>
2021-03-02 08:53:35 -0600

    PrimerPush
    Probando el repositorio

---
+++ Assets/Scripts/MoveByTouch.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour
+{
+
+    void Update()
+    {
+        //Con esto si tocamos la pantalla en algun lado se deberia mover el cuadro a esa posicion en la pantalla
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            //Aqui dependiendo el sitio donde toquemos la camara se hace esa conversion
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0f;
+            transform.position = touchPosition;
+        }
+    }
+}
